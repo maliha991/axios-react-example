0a1d3b0f89a032959ca09cea28f515f4524a1b39
maliha991 <maliha.mithila24@gmail.com>
2022-04-13 18:18:44 +0600

    fetch products and user info

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,17 +1,24 @@
-import { useEffect } from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
 
-const productsUrl = 'https://course-api.com/react-store-products';
-const randomUserUrl = 'https://randomuser.me/api';
+const productsUrl = "https://course-api.com/react-store-products";
+const randomUserUrl = "https://randomuser.me/api";
 
 const GlobalInstance = () => {
-  const fetchData = async () => {
-    console.log('global axios instance');
-  };
+	const fetchData = async () => {
+		try {
+			const response = await axios.get(randomUserUrl);
+			console.log("Global: ", response);
+		} catch (error) {
+			console.log(error.response);
+		}
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-  return <h2 className='text-center'>global instance</h2>;
+	return <h2 className="text-center">global instance</h2>;
 };
+
 export default GlobalInstance;
