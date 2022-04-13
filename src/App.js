fda70aaa605e38c3a1c6a09f4e467037b83a1621
maliha991 <maliha.mithila24@gmail.com>
2022-04-13 17:27:28 +0600

    add header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Title from "./components/Title";
 import FirstRequest from "./examples/1-first-request";
+import Headers from "./examples/2-headers";
 
 function App() {
 	return (
 		<main>
 			<Title />
 			<FirstRequest />
+			<Headers />
 		</main>
 	);
 }
