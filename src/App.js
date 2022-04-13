dacb34d831e99a1d665e407f4f58c0d193acf40b
maliha991 <maliha.mithila24@gmail.com>
2022-04-13 18:18:12 +0600

    add global instance component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Title from "./components/Title";
 import FirstRequest from "./examples/1-first-request";
 import Headers from "./examples/2-headers";
 import PostRequest from "./examples/3-post-request";
+import GlobalInstance from "./examples/4-global-instance";
+
+import "./axios/global";
 
 function App() {
 	return (
@@ -12,6 +15,7 @@ function App() {
 			<FirstRequest />
 			<Headers />
 			<PostRequest />
+			<GlobalInstance />
 		</main>
 	);
 }
