f18d8102fe23dee60fe3eb2cc945a0d9985a2769
maliha991 <maliha.mithila24@gmail.com>
2022-04-14 17:28:54 +0600

    fetch data using interceptor

--- src/examples/6-interceptors.js
+++ src/examples/6-interceptors.js
@@ -1,16 +1,24 @@
-import { useEffect } from 'react';
+import React, { useEffect } from "react";
+import axios from "axios";
 
-const url = 'https://course-api.com/react-store-products';
+import authFetch from "../axios/interceptors";
+
+const url = "https://course-api.com/react-store-products";
 
 const Interceptors = () => {
-  const fetchData = async () => {
-    console.log('axios interceptors');
-  };
+	const fetchData = async () => {
+		try {
+			const response = await authFetch("/react-store-products");
+		} catch (error) {
+			console.log(error.response);
+		}
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-  return <h2 className='text-center'>interceptors</h2>;
+	return <h2 className="text-center">interceptors</h2>;
 };
+
 export default Interceptors;
