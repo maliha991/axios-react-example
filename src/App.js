00087a50a859812e2c8fb88232aea68a2df9a07f
maliha991 <maliha.mithila24@gmail.com>
2022-04-14 17:27:59 +0600

    add interceptor component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Title from "./components/Title";
 import FirstRequest from "./examples/1-first-request";
 import Headers from "./examples/2-headers";
 import PostRequest from "./examples/3-post-request";
-import GlobalInstance from "./examples/4-global-instance";
-import CustomInstance from "./examples/5-custom-instance";
+// import GlobalInstance from "./examples/4-global-instance";
+// import CustomInstance from "./examples/5-custom-instance";
+import Interceptors from "./examples/6-interceptors";
 
 // import "./axios/global";
 
@@ -16,8 +17,9 @@ function App() {
 			<FirstRequest />
 			<Headers />
 			<PostRequest />
-			<GlobalInstance />
-			<CustomInstance />
+			{/* <GlobalInstance />
+			<CustomInstance /> */}
+			<Interceptors />
 		</main>
 	);
 }
